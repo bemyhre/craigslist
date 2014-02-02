4ca38f793c4f445b768ddd3a1a82c213cee4990c
Benjamin Myhre <ben.myhre35@gmail.com>
2014-02-01 20:27:59 -0600

    Update cljobs.rb
    had wrong url in some of the text. changed ".com" to ".org" line 20.
    
    ln 40 changed =nil to =""
    
    ln 30 changed items to string

--- bin/lib/cljobs.rb
+++ bin/lib/cljobs.rb
@@ -4,23 +4,23 @@ filename = "printing.html"
 script = $0
 
 def get_post_date(listing_url) #this method takes in a page and returns a date - by far the most intensive process
-	listing=Nokogiri::HTML(open(listing_url)).css("p")
+  listing=Nokogiri::HTML(open(listing_url)).css("p")
 	setter=""
 	for element in listing
       if element.css('time').text!=""&&setter==""
       	post_time =Time.parse(element.css('time').text)
   	    return post_time
       end
-    end
+  end
 end
 
 def main_page(location,term) #this takes in a search url and return a hash of links page names and postdates for that locaton
   job_master={}
   location.each do |p|
-    open_search_page=Nokogiri::HTML(open("http://#{p}.craigslist.org/search/jjj?query=#{term}&zoomToPosting=")).css("a")
+    open_search_page=Nokogiri::HTML(open("http://#{p}.craigslist.org/search/jjj?query=#{term}")).css("a")
     for x in open_search_page
       if (x.attr("href")[0]=="/")&&(x.attr("href")[-1]=="l")&&(x.attr("class")!="i")
-          job_master["http://#{p}.craigslist.com#{x.attr("href")}"]={"time"=>get_post_date("http://#{p}.craigslist.com#{x.attr("href")}"), "text"=>x.text, "location"=>p}
+          job_master["http://#{p}.craigslist.org#{x.attr("href")}"]={"time"=>get_post_date("http://#{p}.craigslist.org#{x.attr("href")}"),"text"=>x.text, "location"=>p}
       end
     end
   end
@@ -29,7 +29,7 @@ end
 
 def print_page(location,time,term)
   main_page(location,term).sort_by{|k,v| v["time"]}.reverse.each do |x|
-    if x[1]["time"]>time
+    if x[1]["time"].to_s>time.to_s
       Target.write("Time Posted: #{x[1]["time"]}  <a href='#{x[0]}' target='_blank'>#{x[1]['location']} - #{x[1]["text"]}</a><br/>")
     end
   end
@@ -39,7 +39,7 @@ def get_data()
   loc_entry=[]
   print "Enter one search word (or enter for default):"
   term=gets.chomp()
-  if term==nil
+  if term==""
     term="ruby"
   end
   keeper=1
@@ -62,8 +62,3 @@ t=Time.now-200000
 Target.write("<html><title>Recent Multiple Location search results</title><body>")
 print_page(location,t,term)
 Target.write("</body></html>")
-
-
-
-
-
